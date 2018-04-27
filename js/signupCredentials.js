bfea8b381b0649a77408f22d975b83076ca50767
abhinavk123 <abhinavvv123@gmail.com>
2018-04-27 23:20:33 +0530

    added function to display content of book

--- js/signupCredentials.js
+++ js/signupCredentials.js
@@ -4,14 +4,15 @@ var database;
 var txt = '';
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
-  if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
-    txt = xmlhttp.responseText;
-    database = JSON.parse(txt);
-  }
+    if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+        txt = xmlhttp.responseText;
+        database = JSON.parse(txt);
+    }
 };
 
 xmlhttp.open("GET","json/json.txt",true);
 xmlhttp.send();
+
 
 function getDetails() {
 
@@ -58,5 +59,51 @@ function loginVerify() {
 }
 
 function onLogin() {
+    updateContent();
+}
+
+function updateContent() {
+     var prevCode = '<div class="row">' +
+        '<div id ="content" class="col-sm-2">' +
+            '<h1><i> &nbsp; Content</i></h1>' +
+            '<ul id = "grade" style="list-style-type:disc">';
+            var i;
+            var temp = "";
+            for(i = 0; i< database.gradesList.length ; i++) {
+
+                temp += '<li>' + database.gradesList[i].name;
+                var j = "";
+                temp += '<ul id = "subject'+i+'">';
+                var temp2 = "";
+                for (j = 0; j < database.gradesList[i].subjectList.length; j++) {
+
+                    temp2 += '<li>' + database.gradesList[i].subjectList[j].name +
+                        '<ul id = "chapter'+i+j+'">';
+                    var k;
+                    var temp3 = "";
+                    for (k = 0; k < database.gradesList[i].subjectList[j].chapterList.length; k++) {
+                        temp3 += '<li><a onclick="updateChapterQa(i,j,k)">' + database.gradesList[i].subjectList[j].chapterList[k].name + '</a></li>';
+                    }
+                    temp2 = temp2.concat(temp3);
+                    temp2 += '</ul>';
+                    temp2 += '</li>';
+                }
+                temp = temp.concat(temp2);
+                temp += '</ul>';
+
+                temp += '</li>';
+            }
+            temp += '</ul>'+
+        '</div>' +
+        '<div id =chapterDetails" class="col-sm-10">.col-sm-4</div>' +
+    '</div>';
+    var myhtmlcode = prevCode.concat(temp);
+    document.getElementById('innerbody').innerHTML=myhtmlcode;
+document.getElementById('innerbody').style.backgroundColor="#ffffff";
+
+}
+
+function updateChapterQa(i,j,k) {
+    document.getElementById('chapterDetails').innerHTML="Hello"+i+j+k;
 
 }
